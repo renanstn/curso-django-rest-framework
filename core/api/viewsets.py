b894dd34bbcf8b0fa60212d0478bf1ef29890c86
Doc McCoy <renan_ota@hotmail.com>
2020-02-28 00:21:41 -0300

    Sobrescrevendo métodos do ViewSet

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -8,3 +9,27 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    # def list(self, request, *args, **kwargs):
+    #     '''Método disparado no GET geral'''
+    #     return Response({'teste': 123})
+
+    # def create(self, request, *args, **kwargs):
+    #     '''Método disparado no POST'''
+    #     pass
+
+    # def destroy(self, request, *args, **kwargs):
+    #     '''Método disparado no DELETE'''
+    #     pass
+
+    # def retrieve(self, request, *args, **kwargs):
+    #     '''Método disparado no GET de um recurso específico'''
+    #     pass
+
+    # def update(self, request, *args, **kwargs):
+    #     '''Método disparado no PUT'''
+    #     pass
+
+    # def partial_update(self, request, *args, **kwargs):
+    #     '''Método disparado no PATCH'''
+    #     pass
