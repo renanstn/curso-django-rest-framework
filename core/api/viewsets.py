cc07d7f80246045249cd55b8645898a5dc6e603d
Doc McCoy <renan_ota@hotmail.com>
2020-02-28 00:30:29 -0300

    Implementadas actions personalizadas

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -33,3 +34,13 @@ class PontoTuristicoViewSet(ModelViewSet):
     # def partial_update(self, request, *args, **kwargs):
     #     '''Método disparado no PATCH'''
     #     pass
+
+    @action(methods=['get'], detail=True) # detail=True torna a PK acessível
+    def denunciar(self, request, pk=None):
+        '''Action personalizada, pode ser acessada com: /pontoturistico/1/denunciar/'''
+        return Response({'id':pk})
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request, pk=None):
+        '''Action personalizada, pode ser acessada com: /pontoturistico/teste/'''
+        return Response({'helo': 'world'})
