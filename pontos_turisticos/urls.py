0684c1ee6322dbcf112a3474c8895681733b4691
Doc McCoy <renan_ota@hotmail.com>
2020-02-27 23:49:09 -0300

    Criado endpoint Avaliacoes

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -6,6 +6,7 @@ from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecosViewSet
 from comentarios.api.viewsets import ComentariosViewSet
+from avaliacoes.api.viewsets import AvaliacoesViewSet
 
 
 router = routers.DefaultRouter()
@@ -13,6 +14,7 @@ router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecosViewSet)
 router.register(r'comentarios', ComentariosViewSet)
+router.register(r'avaliacoes', AvaliacoesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
