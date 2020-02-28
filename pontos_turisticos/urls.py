41568e9e12a1c8d6b9dbb9b2925e50ce9d0f6b0a
Doc McCoy <renan_ota@hotmail.com>
2020-02-27 23:43:49 -0300

    Adicionado endpoint comentarios

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -5,12 +5,14 @@ from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecosViewSet
+from comentarios.api.viewsets import ComentariosViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'enderecos', EnderecosViewSet)
+router.register(r'comentarios', ComentariosViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
