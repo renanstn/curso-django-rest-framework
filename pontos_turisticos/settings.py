beacee0abffb0bae7c718d7ae624cee5ea6421f0
Renan Santana <renan_ota@hotmail.com>
2020-03-30 23:29:22 -0300

    Autenticação desabilitada para teste

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -55,7 +55,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
