ca732999509945d8c32db81ea71249c6c0a3271e
Renan Santana <renan_ota@hotmail.com>
2020-04-02 23:51:38 -0300

    App agora utiliza o postgres-heroku

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,6 @@ MEDIA_URL = '/media/'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+# Configurar banco de dados para utilizar o postgres-heroku
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
